ca7a2c6de1108853a3a77c6c6524cbfb53f7ecf8
supcraf <112123747+supcraf@users.noreply.github.com>
2022-10-26 14:03:49 +0200

    Update settings.ts

--- src/settings.ts
+++ src/settings.ts
@@ -1,7 +1,7 @@
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = 'ajax-plugin';
+export const PLATFORM_NAME = 'homebridge-ajax-status';
 
 /**
  * This must match the name of your plugin as defined the package.json
